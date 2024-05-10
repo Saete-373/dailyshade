1565ef11233be6603967ae3f2988d3ec628331e0
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2024-05-10 13:56:49 +0700

    แก้class

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -77,7 +77,7 @@ function DeleteModal(props) {
                   <button
                     type="button"
                     onClick={() => props.setTrigger(false)}
-                    class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                    className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                   >
                     Cancel
                   </button>
